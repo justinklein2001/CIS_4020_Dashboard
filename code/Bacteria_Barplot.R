e4cd0143c094315f92eab2d054874002048f7557
mcgunath <mcgunath@gmail.com>
2023-11-15 22:09:56 -0500

    created Naive Bayesian search up

--- code/Bacteria_Barplot.R
+++ code/Bacteria_Barplot.R
@@ -14,12 +14,14 @@ library(ggplot2)
 amr_data_file <- "./data/AMR Data.parquet"
 drug_tiers_data_file <- "./data/Drug Tiers.csv"
 
+drug_tiers <- read_csv(drug_tiers_data_file)
+
 parquet_file <- arrow::read_parquet(amr_data_file)
 #print(unique(parquet_file$R1))
 
 # Define the predictor variables (last 57 columns)
 antibiotics <- colnames(parquet_file)[(ncol(parquet_file) - 56):ncol(parquet_file)]
-bac_anti <- c("org_standard", antibiotics)
+bac_anti <- c("org_standard", "species", antibiotics)
 
 bac_anti <- parquet_file[, bac_anti]
 # Find columns with all "NA" values
@@ -30,14 +32,10 @@ bac_anti <- bac_anti[, !na_only_columns, drop = FALSE]
 bac_anti <- bac_anti[!is.na(bac_anti$org_standard), ]
 bac_anti[is.na(bac_anti)] <- "NA"
 
-num_bacteria = n_distinct(bac_anti$org_standard)
-
-temp <- bac_anti[,!(names(bac_anti) == "org_standard")]
-
 data_rows <- length(unique(bac_anti$org_standard))
 row_names <- unique(bac_anti$org_standard)
-data_columns <- ncol(bac_anti[,names(bac_anti) != "org_standard"])
-col_names <- colnames(bac_anti[,names(bac_anti) != "org_standard"])
+data_columns <- ncol(bac_anti[,!names(bac_anti) %in% c("org_standard", "species")])
+col_names <- colnames(bac_anti[,!names(bac_anti) %in% c("org_standard", "species")])
 
 sub_collection <- data.frame(matrix(0, data_rows, data_columns))
 colnames(sub_collection) <- col_names
@@ -51,16 +49,34 @@ total_collection <- data.frame(matrix(0, data_rows, data_columns))
 colnames(total_collection) <- col_names
 total_collection$org_standard <- row_names
 
+dog_sub <- data.frame(matrix(0, data_rows, data_columns))
+colnames(dog_sub) <- col_names
+dog_sub$org_standard <- row_names
+
+dog_res <- data.frame(matrix(0, data_rows, data_columns))
+colnames(dog_res) <- col_names
+dog_res$org_standard <- row_names
+
 for (i in colnames(bac_anti)){
-  if (i == "org_standard") next
+  if (i == "org_standard" | i == "species") next
   
   anti <- bac_anti[i]
+  anti$dogs <- bac_anti$species
   anti[anti == "S"] <- 1
-  anti[anti == "R" | anti == "I"] <- 0
+  anti$dogs <- anti[,i] == 1 & anti$dogs == "CANINE"
+  anti[anti == "R" | anti == "I" | anti == FALSE] <- 0
   anti$org_standard <- bac_anti$org_standard
-  anti <- anti[anti[i] == 1 | anti[i] == 0,]
+  anti <- anti[anti[,i] == 1 | anti[,i] == 0,]
   sub_sums <- rowsum(as.numeric(anti[,i]), group = anti$org_standard)
   colnames(sub_sums) <- i
+  dog_sums <- rowsum(as.numeric(anti[,"dogs"]), group = anti$org_standard)
+  colnames(dog_sums) <- i
+  
+  for (j in rownames(dog_sums)){
+    dog_sub[dog_sub$org_standard == j, 
+                   names(dog_sub) == colnames(dog_sums)] <- (dog_sub[dog_sub$org_standard == j, 
+                                                                                   names(dog_sub) == colnames(dog_sums)] + dog_sums[rownames(dog_sums) == j,])
+  }
   
   for (j in rownames(sub_sums)){
     sub_collection[sub_collection$org_standard == j, 
@@ -70,15 +86,24 @@ for (i in colnames(bac_anti)){
                    names(total_collection) == colnames(sub_sums)] <- (total_collection[total_collection$org_standard == j, 
                                                                                    names(total_collection) == colnames(sub_sums)] + sub_sums[rownames(sub_sums) == j,])
   }
-  #print(sub_collection)
   
   anti <- bac_anti[i]
-  anti[anti == "S"] <- 0
+  anti$dogs <- bac_anti$species
   anti[anti == "R" | anti == "I"] <- 1
+  anti$dogs <- anti[,i] == 1 & anti$dogs == "CANINE"
+  anti[anti == "S" | anti == FALSE] <- 0
   anti$org_standard <- bac_anti$org_standard
-  anti <- anti[anti[i] == 1 | anti[i] == 0,]
+  anti <- anti[anti[,i] == 1 | anti[,i] == 0,]
   res_sums <- rowsum(as.numeric(anti[,i]), group = anti$org_standard)
   colnames(res_sums) <- i
+  dog_sums <- rowsum(as.numeric(anti[,"dogs"]), group = anti$org_standard)
+  colnames(dog_sums) <- i
+  
+  for (j in rownames(dog_sums)){
+    dog_res[dog_res$org_standard == j, 
+            names(dog_res) == colnames(dog_sums)] <- (dog_res[dog_res$org_standard == j, 
+                                                              names(dog_res) == colnames(dog_sums)] + dog_sums[rownames(dog_sums) == j,])
+  }
   
   for (j in rownames(res_sums)){
     res_collection[res_collection$org_standard == j, 
@@ -88,12 +113,12 @@ for (i in colnames(bac_anti)){
                    names(total_collection) == colnames(res_sums)] <- (total_collection[total_collection$org_standard == j, 
                                                                                    names(total_collection) == colnames(res_sums)] + res_sums[rownames(res_sums) == j,])
   }
-  
+
 }
 
-sub_collection$bac_sum <- rowSums(sub_collection[,names(sub_collection) != "org_standard"])
-res_collection$bac_sum <- rowSums(res_collection[,names(res_collection) != "org_standard"])
-total_collection$bac_sum <- rowSums(total_collection[,names(total_collection) != "org_standard"])
+sub_collection$bac_sum <- rowSums(sub_collection[,!names(sub_collection) %in% c("org_standard", "species")])
+res_collection$bac_sum <- rowSums(res_collection[,!names(res_collection) %in% c("org_standard", "species")])
+total_collection$bac_sum <- rowSums(total_collection[,!names(total_collection) %in% c("org_standard", "species")])
 
 res_collection$prob <- (res_collection$bac_sum / total_collection$bac_sum) * 100
 
@@ -125,5 +150,55 @@ sub_descending <- ggplot(sub_collection, aes(x=reorder(org_standard, -prob),
 sub_descending + ggtitle("Subseceptibility Percentage by Bacteria") +
   xlab("Bacteria Type") + ylab("Antibiotics Resisted %") + ylim(0, 100)
 
+#Naive Bayes Probabilities
+dog_sub <- dog_sub[,!names(dog_sub) %in% c("org_standard")]
+cat_sub <- sub_collection[, !names(sub_collection) %in% c("org_standard", "prob", "bac_sum")] - dog_sub
+dog_res <- dog_res[,!names(dog_res) %in% c("org_standard")]
+cat_res <- res_collection[, !names(res_collection) %in% c("org_standard", "prob", "bac_sum")] - dog_res
+
+total_cats <- cat_sub + cat_res
+total_dogs <- dog_sub + dog_res
+
+total_cats[total_cats < 10] <- 0
+total_dogs[total_dogs < 10] <- 0
+
+cat_probs <- (cat_sub / total_cats) * 100
+dog_probs <- (dog_sub / total_dogs) * 100
+
+cat_probs[is.na(cat_probs)] <- 0
+dog_probs[is.na(dog_probs)] <- 0
+
+cat_probs[cat_probs == Inf] <- 0
+dog_probs[dog_probs == Inf] <- 0
+
+rownames(cat_probs) <- total_collection$org_standard
+rownames(dog_probs) <- total_collection$org_standard
+
+best_antibiotics <- function(bacteria, probs){
+  if (sum(rownames(probs) == bacteria) == 0) {
+    print(sprintf("Bacteria '%s' is not in this list.", bacteria))
+  }
+  best_options <- apply(probs[bacteria,],1,function(x) which(x==max(x)))
+  best_options <- colnames(probs)[best_options]
+  
+  count <- 0
+  for (i in best_options){
+    if (count >= 3) break
+    if (probs[bacteria, i] == 0) break
+    print(sprintf("Antibiotic #%d: %s, Recorded Probability: %f%%", count + 1, i, probs[bacteria, i]))
+    drugs <- i == drug_tiers
+    for (j in 1:nrow(drugs)){
+      if (drugs[j,1]){
+        print(sprintf("Drug Tier: %d", as.numeric(drug_tiers[j,2])))
+      }
+    }
+    count <- count + 1
+  }
+  if (count == 0){
+    print(sprintf("No recorded information for bacteria '%s' on this species.", bacteria))
+  }
+}
+print(rownames(dog_probs))
+best_antibiotics("PSEUDOMONAS LUTEOLA", dog_probs)
 
       
